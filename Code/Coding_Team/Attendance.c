b062bf9c332626c1d9e0387b055c53272400edb4
jeonghwan0458 <63458174+jeonghwan0458@users.noreply.github.com>
2020-05-29 21:18:57 +0900

    Update Attendance.c

--- Code/Coding_Team/Attendance.c
+++ Code/Coding_Team/Attendance.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>                                
 #include <time.h>
-
-int main() {
-	printf("Hello World");
+ 
+int main(){
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+ 
+  printf("now: %d:%d\n",tm.tm_hour, tm.tm_min);
+ 
+  return 0;
 }
